27140f4fcc9ef261d51007ebca1955531940937e
justineAm <78581939+justineAm@users.noreply.github.com>
2021-04-17 17:43:30 +0800

    Exercise number 1 Successfully added!!

---
+++ exercise1.php
@@ -0,0 +1,22 @@
+           
+
+<?php
+function fileLine($filename,$lineNumber){
+    $lines = $filename;
+    $desired_line = $lineNumber;
+    $line_counter = 0; 
+     $fh = fopen('mytext.txt','r') or die($php_errormsg);
+      while ((! feof($fh)) && ($line_counter <= $desired_line)) {
+           if ($s = fgets($fh,1048576)) {
+                $line_counter++; 
+            } 
+        } 
+        fclose($fh) or die($php_errormsg);
+        echo "The text of the line $desired_line of the text file is : ";
+        echo "<br>";
+        print "<h3>$s</h3>"; 
+   
+}
+fileLine("myText",2)
+
+?>
