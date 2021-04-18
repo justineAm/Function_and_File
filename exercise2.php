31ab60d4255e74c92538beb667510af51458137a
justineAm <78581939+justineAm@users.noreply.github.com>
2021-04-18 16:50:23 +0800

    Exercise number 2 successfully added!

--- exercise2.php
+++ exercise2.php
@@ -1,22 +1,27 @@
 <?php
-    function myExistingfile($filename){
+    function myExistingfile($filename,$fileData,$lineNumber){
         if(file_exists($filename)){
-             $file = fopen($filename,'a');
-             fwrite($file,"\nThis the line that is to be inserted to the files soon.");
-             fclose($file);
+            $file = fopen($filename,'a');
+            $lines = file($filename,FILE_IGNORE_NEW_LINES);
+            array_splice( $lines, $lineNumber-1, 0, $fileData );
+            file_put_contents($filename,join("\n",($lines)));
+            fclose($file);
 
+        }else{
+            echo "File does not exist!";
         }
     }
     function readFiles($filename){
         if(file_exists($filename)){
             $file = fopen($filename,'r');
             $contents = fread($file, filesize($filename));//read file    
-            echo "<pre>$contents</pre>";//printing data of file  
+            echo "<pre><h1>$contents</h1></pre>";//printing data of file 
+            echo $contents[4]; 
             fclose($file);
 
        }
     }
     readFiles('myText.txt');
-    myExistingfile('myText.txt')
+    myExistingfile('myText.txt',"This is the inserted Text !",3)
 
 ?>
