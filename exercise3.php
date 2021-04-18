f3c12ae0e6bb82042f9e88b6fda1fff91d2f9678
justineAm <78581939+justineAm@users.noreply.github.com>
2021-04-18 16:49:04 +0800

    Exercise number 3 successfully added!

--- exercise3.php
+++ exercise3.php
@@ -23,12 +23,12 @@ function randomLiner($filename){
     
     $alllines = file($file); 
     if($count%2!=0){
-        echo "The number of vowels are $count<br><br>";
-        echo "The 3rd Line of the file is <br>";
+        echo "<h1>The number of vowels are $count<br><br></h1>";
+        echo "<h1>The 3rd Line of the file is <br></h1>";
         echo ("<h3>$alllines[2]</h3>");
     }else{
-        echo "The number of vowels are $count<br>";
-        echo ($alllines[3]);
+        echo "<h1>The number of vowels are $count<br></h1>";
+        echo ("<h1>$alllines[3]</h1>");
     }
 
     }
@@ -36,7 +36,7 @@ function randomLiner($filename){
         if(file_exists($filename)){
             $file = fopen($filename,'r');
             $contents = fread($file, filesize($filename));//read file    
-            echo "<pre>$contents</pre>";//printing data of file  
+            echo "<pre><h1>$contents</h1></pre>";//printing data of file  
             fclose($file);
 
        }
